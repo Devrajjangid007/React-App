b60efa3146820c53e9c695b9ec72902ef42d2ff6
Devraj jangid <reelstart123@gmail.com>
2026-05-16 22:21:55 +0530

    pages link

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
+import Contect from './Pages/Contect'
+import Product from './Pages/Product'
+import Home from './Pages/Home'
+
+
 const App = () => {
   return (
     <div className='text-white'>
 
-      <Routes>
+       <Routes>
+
+        <Route path='/contect' element={<Contect/>} />
+        <Route path='/product' element={<Product/>} />
+        <Route path='/' element={<Home/>} />
+
+
+
       </Routes>
+
+     
+      
       
       
       </div>
