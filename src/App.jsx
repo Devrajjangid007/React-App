748d911efddbe3ece032ad54e3ab2460c5c43cab
Devraj jangid <reelstart123@gmail.com>
2026-05-16 22:09:22 +0530

    Page remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Contect from './pages/Contect'
-import Home from './pages/Home'
-import Project from './pages/Project'
 
 const App = () => {
   return (
     <div className='text-white'>
 
       <Routes>
-        <Route path='/contect' element={<Contect/>} />
-        <Route path='/' element={<Home/>} />
-        <Route path='/project' element={<Project/>} />
       </Routes>
       
       
